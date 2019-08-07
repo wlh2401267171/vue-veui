785cc06b2327bf96e06e0fb22f2e4d69512651f7
ansel@kobe <2401267171@qq.com>
2019-08-07 20:15:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!--模板html结构-->
+<template>
+ <h1 class="demo">{{msg}}</h1>
+</template>
+<!--负责语法-->
+<script >
+	//export模块化的导出
+	export default {
+		data:function(){
+			return{
+				msg:'王炼辉1'
+			};
+			
+		},methods:{},
+		watch:{},
+	}
+	
+</script>
+<!--负责样式-->
+<style lang="less">
+.demo{
+	color: red;
+}
+</style>
